d036f728d8b6669ffa4c23dbc86829363c5e5fb8
ghanashyamsunuwar <janamsunuwar13@gmail.com>
2024-12-28 15:32:50 +0545

    two sum add

---
+++ Easy/TwoSum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for(i=0; i<nums.length; i++)
+    {
+        let rem = target - nums[i];
+        if(map.has(rem))
+        {
+            return[map.get(rem),i]
+        }
+        else 
+        map.set(nums[i],i)
+    }
+};
